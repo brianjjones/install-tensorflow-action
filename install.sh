5989426c85325cb35d929f23b8d57b85d4805892
Brian Jones <brian.j.jones@intel.com>
2022-09-21 16:27:35 -0700

    dev

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ if [ "$#" -ne 1 ]; then
     version="2.9.1"
 else
     version="$1"
-
-FILENAME=libtensorflow-cpu-linux-x86_64-$version.tar.gz
+fi
+FILENAME="libtensorflow-cpu-linux-x86_64-$version.tar.gz"
+echo "Using $FILENAME"
 wget -q --no-check-certificate https://storage.googleapis.com/tensorflow/libtensorflow/${FILENAME}
 sudo tar -C /usr/local -xzf ${FILENAME}
 sudo ldconfig /usr/local/lib
